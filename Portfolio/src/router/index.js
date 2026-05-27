b93169c16b1dc4a5a19d538ea6858bd94aedfb36
DanielFeoktistovPXL <daniel.feoktistov@student.pxl.be>
2026-05-28 01:33:24 +0200

    gefixed

--- Portfolio/src/router/index.js
+++ Portfolio/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import { wplSections } from '../data/portfolio'
 import ContactView from '../views/ContactView.vue'
@@ -51,7 +51,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 })
 
